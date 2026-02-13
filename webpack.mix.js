3030391ba36a6370821dc5dc8e7cd82079d8fcb1
cheker-135 <hafoun9999@gmail.com>
2026-02-13 07:42:17 -0800

    Fix Vue loader for Laravel Mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
+
+
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+   .vue() // <-- ADD THIS
+   .sass('resources/sass/app.scss', 'public/css');
+
