bb864ad3361cfc86a3a490e9b1a91f685a77b5ef
cheker-135 <hafoun9999@gmail.com>
2026-02-26 00:31:55 -0800

    Update phone number to +21654723656 (no space) and update seeder/sql dump

--- database/seeders/SettingTableSeeder.php
+++ database/seeders/SettingTableSeeder.php
@@ -20,9 +20,9 @@ class SettingTableSeeder extends Seeder
             'short_des'=>"Praesent dapibus, neque id cursus ucibus, tortor neque egestas augue, magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.",
             'photo'=>"image.jpg",
             'logo'=>'logo.jpg',
-            'address'=>"NO. 342 - London Oxford Street, 012 United Kingdom",
-            'email'=>"eshop@gmail.com",
-            'phone'=>"+060 (800) 801-582",
+            'address'=>"Hammam Lif, Tunis, Tunisie",
+            'email'=>"shop@khayrat.com",
+            'phone'=>"+216 54723656",
         );
         DB::table('settings')->insert($data);
     }
